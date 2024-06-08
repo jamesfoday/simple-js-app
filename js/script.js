a7ac63c6661691cf828dec8e87ac5d65615b0d0e
jamesfoday <jamesfodayofficial@gmail.com>
2024-06-08 13:58:38 +0200

    Remove all code from script.js
    All code in script.js has been removed. This commit cleans up the file in preparation for new implementation.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-alert('Hello world')
-let favouriteFood = 'rice';
-document.write(favouriteFood);
+let annesAge = 27;
+let anne = {
+  name: 'Anne',
+  age: annesAge,
+  child: {
+    name: 'Joe',
+    age: 2
+  }
+};
+
+let car = {
+  color: 'red',
+  mileage: 10
+};
