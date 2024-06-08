fb9587415ec63c67f875b4c29bceba2cb4494e5b
jamesfoday <jamesfodayofficial@gmail.com>
2024-06-08 15:49:23 +0200

    script.js updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let pokemonlist=[ {name:"balbasure" height: 0.7 type: ["grass","poison"]}, 
-{name:"Charizard" height: 1.7  type: ["fire","flying"]}, {name:"Charmander" height: 0.6  type:"fire"}];
-
-
+let pokemonlist = [
+  { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
+  { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
+  { name: "Charmander", height: 0.6, type: ["fire"] }
+];
