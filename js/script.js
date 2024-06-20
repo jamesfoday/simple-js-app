226a408bb08b85420bd54be8991aae3272da1117
jamesfoday <jamesfodayofficial@gmail.com>
2024-06-20 19:25:58 +0200

    IIFE created

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 let pokemonRepository = (function() {
-  
-}
+  let pokemonlist = [
+    { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
+    { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
+    { name: "Charmander", height: 0.6, type: ["fire"] }
+  ];
+
+  function getAll() {
+    return pokemonlist;
+  }
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'height' in pokemon &&
+      'type' in pokemon
+    ) {
+      pokemonlist.push(pokemon);
+    } else {
+      console.log("Invalid Pokemon");
+    }
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };  
+})();
 
 
 
