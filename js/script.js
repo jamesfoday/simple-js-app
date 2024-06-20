62b1a8357816ccff64a964a337d644926995d7c5
jamesfoday <jamesfodayofficial@gmail.com>
2024-06-20 19:37:44 +0200

    getAll function returned by the IIFE to retrieve the pokemonlist array started

--- js/script.js
+++ js/script.js
@@ -31,22 +31,6 @@ let pokemonRepository = (function() {
 
 
 
-let pokemonlist = [
-  { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
-  { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
-  { name: "Charmander", height: 0.6, type: ["fire"] }
-];
-
-let text = "";
-let i = 0;
-
-pokemonlist.forEach(function(pokemon) {
-  text += pokemon.name + " (height: " + pokemon.height + ") ";
-  if (pokemon.height > 1.5) {
-    text += "Wow that’s big ";
-  }
-  text += "<br>"; 
-});
-
+pokemonRepository.getAll().forEach(function(pokemon)
 
 document.write(text);
